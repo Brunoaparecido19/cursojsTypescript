d9f3c5cc592f9def988bd2bb526cb99dd2d1b8a8
Brunoaparecido19 <ap24201@icloud.com>
2022-02-12 14:29:03 -0300

    type-literals

---
+++ src/A0012-types-literals/A0012-types-literals.ts
@@ -0,0 +1,14 @@
+let x: number = 10; // eslint-disable-line
+x = 0b1010;
+
+const y = 10;
+
+const person = {
+  name: 'Bruno' as const,
+  lastName: 'Aparecido',
+};
+function colorRand(cor: 'vermelho' | 'amarelo' | 'azul'): string {
+  return cor;
+}
+console.log(colorRand('vermelho'));
+export default 1;
