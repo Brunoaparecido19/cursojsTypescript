b01e66615d753c27fd7cb6a4b6c774aa710972e7
Brunoaparecido19 <ap24201@icloud.com>
2022-02-13 14:47:33 -0300

    exercicio parcialmente finalizado

---
+++ src/A0019-exercicio/form-control.ts
@@ -0,0 +1,61 @@
+import isEmail from 'validator/lib/isEmail';
+
+const SHOW_ERROR_MENSAGES = 'show-error-message';
+const form = document.querySelector('#form') as HTMLFormElement;
+const userName = document.querySelector('#user-name') as HTMLInputElement;
+const lastName = document.querySelector('#last-name') as HTMLInputElement;
+const email = document.querySelector('#email') as HTMLInputElement;
+const password = document.querySelector('#password') as HTMLInputElement;
+const repeatPassword = document.querySelector(
+  '#repeat-password',
+) as HTMLInputElement;
+
+form.addEventListener('submit', function (e: Event) {
+  e.preventDefault();
+  hideErrorMessage(this);
+  checkForEmptyFields(userName, lastName, password, repeatPassword, email);
+  checkEmail(email);
+  checkEqualPasswords(password, repeatPassword);
+  if (shouldSendForm(this)) console.log('formulario enviado');
+});
+function checkEmail(input: HTMLInputElement): void {
+  if (!isEmail(input.value)) return showErrorMensage(input, 'E-mail inválido');
+}
+function checkEqualPasswords(
+  password: HTMLInputElement,
+  repeatPassword: HTMLInputElement,
+): void {
+  if (password.value !== repeatPassword.value) {
+    showErrorMensage(password, 'Senha incompatível');
+    showErrorMensage(repeatPassword, 'Senha incompatível');
+  }
+}
+function checkForEmptyFields(...inputs: HTMLInputElement[]): void {
+  inputs.forEach((input) => {
+    if (input.value === '') {
+      showErrorMensage(input, 'Preencha este campo');
+    }
+  });
+}
+function hideErrorMessage(form: HTMLFormElement): void {
+  form
+    .querySelectorAll('.' + SHOW_ERROR_MENSAGES)
+    .forEach((item) => item.classList.remove(SHOW_ERROR_MENSAGES));
+}
+function showErrorMensage(input: HTMLInputElement, message: string): void {
+  const formFields = input.parentElement as HTMLDivElement;
+  const errorMessage = formFields.querySelector(
+    '.error-message',
+  ) as HTMLSpanElement;
+  errorMessage.innerText = message;
+  formFields.classList.add(SHOW_ERROR_MENSAGES);
+}
+
+function shouldSendForm(form: HTMLFormElement): boolean {
+  // eslint-disable-next-line prefer-const
+  let send = true;
+  form
+    .querySelectorAll('.' + SHOW_ERROR_MENSAGES)
+    .forEach(() => send === false);
+  return send;
+}
