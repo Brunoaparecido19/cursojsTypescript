106a2012da74c784f644d5fad79d9afcc85e494d
Brunoaparecido19 <ap24201@icloud.com>
2022-03-30 14:30:44 -0300

    Criando um namespace

---
+++ src/A0060-namespace/index.ts
@@ -0,0 +1,19 @@
+// eslint-disable-next-line @typescript-eslint/no-namespace
+export namespace MeuNomeSpace {
+  const nomeDoNamespace = 'Bruno';
+
+  export class PessoaDoNomespace {
+    constructor(public nome: string) {}
+  }
+
+  const pessoaDoNamespace = new PessoaDoNomespace(nomeDoNamespace);
+  console.log(pessoaDoNamespace);
+
+  // eslint-disable-next-line @typescript-eslint/no-namespace
+  export namespace OutroNamespace {
+    export const nomeDoNamespace = 'Gustavo';
+  }
+}
+
+const pessoaDoNamespace = new MeuNomeSpace.PessoaDoNomespace('outro nomespace');
+console.log(pessoaDoNamespace);
